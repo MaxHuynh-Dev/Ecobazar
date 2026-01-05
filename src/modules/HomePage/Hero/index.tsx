1297c4fb3adbee12234831116f06a47bd2eabd0d
max <max>
2026-01-05 16:44:41 +0700

    Refactor Hero component: update layout styles for main, secondary, and tertiary content sections, add absolute positioning for content, and integrate typography for improved visual hierarchy.

--- src/modules/HomePage/Hero/index.tsx
+++ src/modules/HomePage/Hero/index.tsx
@@ -2,6 +2,7 @@ import type React from 'react';
 
 import { Container, GridContainer } from '@/components/Container';
 import ImagePlaceHolder from '@/components/ImagePlaceHolder';
+import Text from '@/components/Typo';
 import classNames from 'classnames';
 import styles from './hero.module.scss';
 
@@ -21,9 +22,14 @@ function Hero(): React.ReactElement {
                 height={600}
               />
             </div>
+            <div className={styles.hero_mainContent_content}>
+              <Text Comp="h1" size={48} weight="semibold" transform="capitalize" color="white">
+                Fresh & Healthy <br /> Organic Food
+              </Text>
+            </div>
           </div>
           <div className={styles.hero_secondaryContent}>
-            <div className={classNames(styles.hero_smallImage, styles.hero_secondaryContent_image)}>
+            <div className={classNames(styles.hero_smallImage)}>
               <ImagePlaceHolder
                 src={
                   'https://images.unsplash.com/photo-1726241966334-6e9f1de905d6?q=80&w=3197&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -35,7 +41,7 @@ function Hero(): React.ReactElement {
             </div>
           </div>
           <div className={styles.hero_tertiaryContent}>
-            <div className={classNames(styles.hero_smallImage, styles.hero_tertiaryContent_image)}>
+            <div className={classNames(styles.hero_smallImage)}>
               <ImagePlaceHolder
                 src={
                   'https://images.unsplash.com/photo-1726241966334-6e9f1de905d6?q=80&w=3197&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
